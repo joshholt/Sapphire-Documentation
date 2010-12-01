004302beec958acac661f0601d2dc7c5fc153c84
Joshua Holt <holt.josh@gmail.com>
2010-12-01 18:02:36 -0500

    Initial Commit of Sapphire Docs

---
+++ app.js
@@ -0,0 +1,23 @@
+/*globals __dirname*/
+/**
+ * Module dependencies.
+ */
+var Connect = require('connect'), app;
+
+app = Connect.createServer(
+ Connect.logger(),
+ Connect.conditionalGet(),
+ Connect.favicon(),
+ Connect.cache(),
+ Connect.gzip(),
+ require('wheat')(__dirname)
+);
+
+module.exports = app;
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Connect server listening on port %d", app.address().port);
+}
---
+++ authors/Josh Holt.markdown
@@ -0,0 +1,4 @@
+Email:    holt.josh@gmail.com
+Github: 	joshholt
+Twitter: 	cjk2121
+Location: Washington, D.C
